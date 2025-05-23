879c350af5edf098bd5eeaffddc715565764c7d0
VitoriaMoro <morovitoria1@gmail.com>
2025-05-23 10:49:27 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,3 +8,7 @@ Hello *world!*
 """)
 st.text_input("Digite seu nome:")
 st.text_input("Digite o seu sobrenome:")
+st.text_input("Digite a sua idade:")
+st.text_input("Digite o seu email:")
+st.text_input("Digite o seu CPF:")
+st.write("Seu cadastro esta completo, clique no botão abaixo para avançar!")
